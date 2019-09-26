de431ad748de041a6b7f71039ef4e651a6671779
Benz56 <bemmamin@live.dk>
2019-09-26 16:01:22 +0200

    1.2.1

--- src/main/java/com/benzoft/commandnotifier/DiscordHook.java
+++ src/main/java/com/benzoft/commandnotifier/DiscordHook.java
@@ -1,24 +1,40 @@
 package com.benzoft.commandnotifier;
 
 import com.benzoft.commandnotifier.persistence.ConfigFile;
-import com.github.kevinsawicki.http.HttpRequest;
 import org.bukkit.Bukkit;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class DiscordHook {
 
     private final CommandNotifier commandNotifier;
 
-    public DiscordHook(final CommandNotifier commandNotifier) {
+    DiscordHook(final CommandNotifier commandNotifier) {
         this.commandNotifier = commandNotifier;
     }
 
     public void sendCommand(final String command) {
-        final String WebhookURL = ConfigFile.getInstance().getWebhookURL();
-        if (WebhookURL.isEmpty()) return;
-        Bukkit.getScheduler().runTaskAsynchronously(commandNotifier, () ->
-                HttpRequest.post(WebhookURL)
-                        .acceptJson()
-                        .header("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11")
-                        .send("{\"content\":\"" + command + "\"}").code());
+        final String webhookURL = ConfigFile.getInstance().getWebhookURL();
+        if (webhookURL.isEmpty()) return;
+
+        Bukkit.getScheduler().runTaskAsynchronously(commandNotifier, () -> {
+            try {
+                final HttpsURLConnection connection = (HttpsURLConnection) new URL(webhookURL).openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11");
+                connection.setDoOutput(true);
+                try (final OutputStream outputStream = connection.getOutputStream()) {
+                    outputStream.write(("{\"content\":\"" + command + "\"}").getBytes(StandardCharsets.UTF_8));
+                }
+                connection.getInputStream();
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
